6588eb3c0048aa948163d5406155280a893ecf6c
Bart Mach <bart.mach@gmail.com>
2019-11-02 22:13:24 -0400

    add topic search to api

--- src/utilities/api.js
+++ src/utilities/api.js
@@ -1,11 +1,16 @@
 
 export const searchByKeyword = async (keyword) => {
   const url= new URL('https://healthfinder.gov/api/v2/topicsearch.json');
+  return getResultsFromApi(url, {keyword})
+}
 
-  const params={
-    api_key: 'demo_api_key',
-    keyword: keyword
-  }
+export const searchByDemographic = async (age, sex) => {
+  const url= new URL('https://healthfinder.gov/api/v2/myhealthfinder.json');
+  return getResultsFromApi(url, {age, sex})
+}
+
+const getResultsFromApi = async (url, params) => {
+  params.api_key = 'demo_api_key';
   url.search = new URLSearchParams(params).toString()
   
   const response = await fetch(url, {referrer: ""});
